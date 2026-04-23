5e83418d968247d39c762c378be39ee55ae4eb24
Crystal <cforourk@umich.edu>
2026-04-23 07:42:42 -0400

    adding some additional comments inside the docs to reinforce understanding

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,9 +1,11 @@
-/* map over the show list to display information about a show when clicked
-
+/* map over the show list to display information about a show when clicked. I add to add the css file to make my changes work right. I also messed up the classnames.
 */
+import "./shows.css"
 function ShowSelection({ shows , setSelectedShow }) {
+/*
+*/
     return (
-    <menu className="showSelect">
+    <nav className="shows">
     {shows.map((show) => (
         <a className="show" key={show.name} onClick={() =>
          setSelectedShow(show)}>{show.name}
@@ -11,7 +13,7 @@ function ShowSelection({ shows , setSelectedShow }) {
          </a>
 
     ))}
-</menu>
+</nav>
     );
 }
 export default ShowSelection
